0ce74f40718e874b50b20b39566dbc9c23987a88
Ayoub-Sahli <82885176+Ayoub-Sahli@users.noreply.github.com>
2022-01-26 22:52:14 +0100

    Update Controls.cs

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Controls : MonoBehaviour
 {
     public float Speed=10f;
+    public float rotationX;
+    public float rotationY;
+    public float sensitivity = 5f;
     [SerializeField] private LineRenderer aimRenderer = default;
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,16 @@ public class Controls : MonoBehaviour
         Deplacement();
         Vise();
     }
+    
+    void Vise()
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY += Input.GetAxis("Mouse X") * sensitivity;
+        
+        rotationX = Mathf.Clamp (rotationX, -90, 90);
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+    }
+    
     
     void Deplacement()
     {
